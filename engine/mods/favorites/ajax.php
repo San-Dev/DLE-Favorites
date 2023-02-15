a5f2da0e03bd2de483ec04e0488da3613bdda475
Sander <oleg.sandev@gmail.com>
2023-02-15 20:08:38 +0200

    Update ajax.php

--- engine/mods/favorites/ajax.php
+++ engine/mods/favorites/ajax.php
@@ -1,10 +1,11 @@
 <?php
-error_reporting(E_ALL ^ E_WARNING ^ E_NOTICE);
-ini_set('display_errors', true);
-ini_set('html_errors', false);
+
+use Sandev\Favorites;
+
+error_reporting(E_ALL ^ E_WARNING ^ E_NOTICE ^ E_DEPRECATED);
 
 define('DATALIFEENGINE', true);
-define('ROOT_DIR', realpath(__DIR__ . '/../../..'));
+define('ROOT_DIR', dirname(__DIR__, 3));
 define('ENGINE_DIR', ROOT_DIR . '/engine');
 
 include_once __DIR__ . '/loader.php';
@@ -16,12 +17,9 @@ setlocale(LC_NUMERIC, "C");
 require_once DLEPlugins::Check(ENGINE_DIR . '/modules/functions.php');
 dle_session();
 require_once DLEPlugins::Check(ENGINE_DIR . '/modules/sitelogin.php');
-if (!$is_logged) {
-	$member_id['user_group'] = 5;
-}
+$is_logged || $member_id = ['user_group' => 5];
 
 require_once __DIR__ . '/class.favorites.php';
-$favmod = new Sandev\Favorites;
 
-$error = $favmod->setId($_POST['newsid']);
+$error = (new Favorites)->setId($_POST['newsid']);
 echo $error ?: json_encode(['result' => true]);
